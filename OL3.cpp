be167ff4c075bcf10c7edb9b2251d318ea6128c6
olegk <kbryzhenyukpc@gmail.com>
2024-12-06 11:53:53 +0200

    Добавьте файлы проекта.

---
+++ OL3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+    int days;
+    std::cout << "кол-во дней ";
+    std::cin >> days;
+
+    int weeks = days / 7;
+    int remainingDays = days % 7;
+
+    std::cout << weeks << " недели " << remainingDays << " дни" << std::endl;
+
+    return 0;
+}
